6dc55e669e8bd5384b72ed5dddc3f64cc5f155da
Flavio Roman <flavioromanweb@gmail.com>
2024-11-20 10:48:56 -0500

    clase 1 webinar

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -7,13 +7,16 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
+import { QrCodeModule } from 'ng-qrcode';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    QrCodeModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  qrText: string = 'Hola Mundo!';
+
   constructor() {}
 
 }
